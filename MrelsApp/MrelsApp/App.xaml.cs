ac9bafa72ae6477dbcb28ea1bed564b36c30ac4b
Manux <manu_cg19@hotmail.com>
2018-03-07 21:49:51 +0100

    App

---
+++ MrelsApp/MrelsApp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using MrelsApp.Views;
+using MrelsApp.Views.WorkoutViews;
+using Xamarin.Forms;
+
+namespace MrelsApp
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ MrelsApp/MrelsApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MrelsApp.Models;
+
+namespace MrelsApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
